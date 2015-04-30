c40d26519f825e08b5a9f231100b89eb0b20aaeb
Lee Joe <joelee@Lees-Mac.local>
2015-04-30 20:20:38 +0800

    Initial Commit

---
+++ HorseRacingGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorseRacingGame
+//
+//  Created by Lee Joe on 4/30/15.
+//  Copyright (c) 2015 Lee Joe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HorseRacingGame/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HorseRacingGame
+//
+//  Created by Lee Joe on 4/30/15.
+//  Copyright (c) 2015 Lee Joe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
